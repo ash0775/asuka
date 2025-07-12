4ad02afca076e4ea837b8946cab0383a41275fc3
asuka <asuukakonno07@gmail.com>
2025-07-12 13:20:38 +0900

    Add:form

--- js/script.js
+++ js/script.js
@@ -27,3 +27,30 @@ const swiper = new Swiper(".swiper", {
     },
   }
 });
+
+  $(function() {
+    // validate
+    $("#contact_form").validate({
+        // Set the validation rules
+        rules: {
+            name: "required",
+            email: {
+                required: true,
+                email: true
+            },
+            message: "required",
+        },
+        // Specify the validation error messages
+        messages: {
+            name: "Please enter your name",
+            email: "Please enter a valid email address",
+            message: "Please enter a message",
+        },
+        // submit handler
+        submitHandler: function(form) {
+          //form.submit();
+           $(".message").show();
+           $(".message").fadeOut(4500);
+        }
+    });
+  });
